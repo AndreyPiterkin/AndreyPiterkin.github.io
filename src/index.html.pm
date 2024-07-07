edd4c9a6612986dfd03ffdc97309200c862a09d4
Andrey Piterkin <andrey.piterkin@gmail.com>
2024-07-07 12:27:35 -0700

    chore: update website to reflect latest info

--- src/index.html.pm
+++ src/index.html.pm
@@ -1,22 +1,27 @@
 #lang pollen
 
 ◊rows[#:gap 0]{
-    ◊p{I am Andrey, and I am a Junior studying Computer Science at Northeastern University. I am passionate about ◊em{systems programming}, ◊em{systematic design}, and ◊em{performant simulation}.}
+    ◊p{I am Andrey, and I am a Senior studying Computer Science at Northeastern University. I am passionate about ◊em{systems programming}, ◊em{systematic design}, and ◊em{performant simulation}.}
     ◊empty-vspace[#:size 2]
+
+    ◊h3{Recently, I was}
+    ◊items{
+        ◊item{Working at ◊link[#:url "https://www.mathworks.com"]{MathWorks} in ◊em{Control Design Automation} for Spring 2024}
+        ◊item{Taking Compiler Design (CS 4410)}
+    }
 
     ◊h3{Right now, I am}
     ◊items{
-        ◊item{Systematically designing a ◊link[#:url "https://www.lukejianu.com/small-rust-os/index.html"]{toy OS} in ◊lang{Rust}}
+        ◊item{Working at ◊link[#:url "https://www.databricks.com"]{Databricks} on the Money team
+            for Summer 2024}
+        ◊item{Systematically designing a ◊link[#:url "https://www.lukejianu.com/toy-rust-os/index.html"]{toy OS} in ◊lang{Rust}}
         ◊item{Creating an ECS-based planet simulator in ◊lang{C++}}
-        ◊item{Working at ◊link[#:url "https://www.mathworks.com"]{MathWorks} in ◊em{Control Design Automation} for Spring 2024}
-        ◊item{Taking Compiler Design (CS 4410)}
+        ◊item{Building a visualization tool for my Compiler}
     }
 
     ◊empty-vspace[#:size 2]
     ◊h3{Soon, I will be}
     ◊items{
-        ◊item{Working at ◊link[#:url "https://www.databricks.com"]{Databricks} 
-            for Summer 2024}
         ◊item{Working at ◊link[#:url "https://www.datadoghq.com"]{Datadog} 
             for Fall 2024}
     }
