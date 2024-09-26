b5d0830fe1d4f6bd2fd5069fe4e798a9b745a3cc
Andrey Piterkin <andrey.piterkin@gmail.com>
2024-09-25 21:20:36 -0400

    feat: move datadog into current

--- src/index.html.pm
+++ src/index.html.pm
@@ -6,24 +6,25 @@
 
     ◊h3{Recently, I was}
     ◊items{
-        ◊item{Working at ◊link[#:url "https://www.mathworks.com"]{MathWorks} in ◊em{Control Design Automation} for Spring 2024}
+        ◊item{Working at ◊link[#:url "https://www.mathworks.com"]{MathWorks} in ◊em{Control Design Automation} for Spring 2024, designing APIs for Matrix operations considering input and output range and reducing C types for generated code.}
         ◊item{Taking Compiler Design (CS 4410)}
+        ◊item{Working at ◊link[#:url "https://www.databricks.com"]{Databricks} on the Money team
+            for Summer 2024, improving developer velocity by inventing a new lost cost simulation framework for billable usage.}
     }
 
     ◊empty-vspace[#:size 2]
     ◊h3{Right now, I am}
     ◊items{
-        ◊item{Working at ◊link[#:url "https://www.databricks.com"]{Databricks} on the Money team
-            for Summer 2024}
         ◊item{Systematically designing a ◊link[#:url "https://www.lukejianu.com/toy-rust-os/index.html"]{toy OS} in ◊lang{Rust}}
         ◊item{Creating an ECS-based planet simulator in ◊lang{C++}}
         ◊item{Building a visualization tool for my Compiler}
+        ◊item{Working at ◊link[#:url "https://www.datadoghq.com"]{Datadog} 
+            for Fall 2024 on the ◊em{Edge Ingress} team.}
     }
 
     ◊empty-vspace[#:size 2]
     ◊h3{Soon, I will be}
     ◊items{
-        ◊item{Working at ◊link[#:url "https://www.datadoghq.com"]{Datadog} 
-            for Fall 2024}
+        ◊item{Resuming school and taking Computer Architecture and Theory of Computation.}
     }
 }
