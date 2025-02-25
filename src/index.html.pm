02c5837f62d672b523d03aaf681a0d6546233fd7
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-02-24 19:03:07 -0500

    remove horrible pseudomacros and replace them with real macros

--- src/index.html.pm
+++ src/index.html.pm
@@ -2,25 +2,25 @@
 
 ~rows[#:gap 0]{
     ~p{I am Andrey, and I am a Senior studying Computer Science at Northeastern University. I am passionate about ~em{systems programming}, ~em{systematic design}, and ~em{simulation}.}
-    ~empty-vspace[#:size 2]
+    ~vempty{2}
 
     ~h3{Recently, I was}
     ~items{
-        Working at ~link[#:url "https://www.mathworks.com"]{MathWorks} in ~em{Control Design Automation} for Spring 2024, improving Simulink C codegen by optimizing type selection
-        Working at ~link[#:url "https://www.databricks.com"]{Databricks} on the ~em{Money} team for Summer 2024, improving load, integration, and chaos testing for the real-time billing pipeline
-        Working at ~link[#:url "https://www.datadoghq.com"]{Datadog} on the ~em{Edge Ingress} team for Fall 2024, enabling search for automation-managed cloud resources
+        ~{Working at ~link[#:url "https://www.mathworks.com"]{MathWorks} in ~em{Control Design Automation} for Spring 2024, improving Simulink C codegen by optimizing type selection}
+        ~{Working at ~link[#:url "https://www.databricks.com"]{Databricks} on the ~em{Money} team for Summer 2024, improving load, integration, and chaos testing for the real-time billing pipeline}
+        ~{Working at ~link[#:url "https://www.datadoghq.com"]{Datadog} on the ~em{Edge Ingress} team for Fall 2024, enabling search for automation-managed cloud resources}
     }
 
-    ~empty-vspace[#:size 2]
+    ~vempty{2}
     ~h3{Right now, I am}
     ~items{
-        Learning Racket's macro system in Hack Your Own Language (CS 3620)
-        Taking Computer Architecture and Theory of Computation
+        ~{Learning Racket's macro system in Hack Your Own Language (CS 3620)}
+        ~{Taking Computer Architecture and Theory of Computation}
     }
 
-    ~empty-vspace[#:size 2]
+    ~vempty{2}
     ~h3{Soon, I will be}
     ~items{
-        Working at Databricks in Seattle full-time
+        ~{Working at Databricks in Seattle full-time}
     }
 }
--- src/pollen.rkt
+++ src/pollen.rkt
@@ -4,56 +4,34 @@
          pollen/decode
          racket/match
          racket/list
-         racket/function)
+         racket/function
+
+         (for-syntax syntax/parse
+                     racket/base
+                     racket/list
+                     racket/function))
 
 (provide (all-defined-out))
+
 (define headline (default-tag-function 'h2 #:class "headline"))
 (define lang (default-tag-function 'em #:style "font-style: normal;
                                    font-weight: 500;"))
-(define items (default-tag-function 'ul #:class "dash"))
+(define list-items (default-tag-function 'ul #:class "dash"))
+(define list-item (default-tag-function 'li))
 
-(struct transformer (pred? transform) #:transparent)
+(define-syntax items
+  (lambda (stx)
+    (syntax-parse stx
+      [(_ (~or* (item ...) (~datum "\n")) ...)
+       (define/syntax-parse ((filtered ...) ...)
+          (filter identity (attribute item)))
+       #'(list-items (apply list-item (list filtered ...)) ...)])))
 
-;; Basically a scuffed, unsanitary macro expander
-;; TODO: figure out how to ensure expansion is uniform, if there is some built in expansion for this
-;; (convert to Racket and macro expand?)
-(define (transform x . transformers)
-  (define (apply-transformers x)
-    (for/fold ([xpr x])
-              ([transformer transformers])
-        (if ((transformer-pred? transformer) x)
-            ((transformer-transform transformer) x)
-            x)))
-  (match x
-    [(list) '()]
-    [(list xs ...)
-     (map (lambda (x) (apply transform x transformers)) (apply-transformers x))]
-    [x (apply-transformers x)]))
-
-(define items-transformer
-  (transformer
-    (lambda (x)
-      (match x
-        [(list 'ul (list (list y ...) ...) z ...)
-         #t]
-        [_ #f]))
-    (lambda (x)
-      (match x
-        [(list 'ul (list (list y ...) ...) sublists ...)
-         (define (split-list l)
-           (let loop ([groups '()]
-                      [lst l])
-             (define split-func (negate (curry equal? "\n")))
-             (define prefix (takef lst split-func))
-             (define suffix (dropf lst split-func))
-             (if (null? prefix)
-                 (reverse groups)
-                 (loop (cons prefix groups) (if (null? suffix) suffix (rest suffix))))))
-         `(ul ,y ,@(map (lambda (s) (cons 'li s)) (split-list sublists)))]
-        [_ (error 'expansion-failure "unexpected pattern: ~v" x)]))))
-
-(define (root . elements)
-  (txexpr 'root empty (decode-elements (transform elements items-transformer))))
+(define-syntax vempty 
+  (lambda (stx)
+    (syntax-parse stx
+      [(_ x:string)
+       #'(empty-vspace #:size x)])))
 
 (module setup racket/base
   (provide (all-defined-out))
@@ -62,30 +40,30 @@
 (define (link #:url url #:target [target "_blank"] text)
   (txexpr 'a `((href ,url) (target ,target)) `(,text)))
 
-(define (rows #:gap [gap 4] #:align [alignment "stretch"] #:class [class
-                                                                    ""] . els)
-    (define txexprs (filter txexpr? els))
-    (define style (format "display: flex; flex-direction: column;
-                          row-gap: ~aem; align-self: ~a;" gap alignment))
-    (txexpr 'div `((class ,class) (style ,style)) (map flex-item
-                          txexprs)))
+
+(define (rows #:gap [gap 4] #:align [alignment "stretch"] #:class [class ""] . els)
+  (define txexprs (filter txexpr? els))
+  (define style (format "display: flex; flex-direction: column;
+                        row-gap: ~aem; align-self: ~a;" gap alignment))
+                        (txexpr 'div `((class ,class) (style ,style)) (map flex-item
+                                                                           txexprs)))
 
 (define (cols #:gap [gap 4] #:align [alignment "stretch"] #:class [class
                                                                     ""] . els)
-    (define txexprs (filter txexpr? els))
-    (define style (format "display: flex; flex-direction: row;
-                          column-gap: ~aem; align-self: ~a;" gap
-                          alignment))
-    (txexpr 'div `((class ,class) (style ,style)) (map flex-item
-                          txexprs)))
+  (define txexprs (filter txexpr? els))
+  (define style (format "display: flex; flex-direction: row;
+                        column-gap: ~aem; align-self: ~a;" gap
+                        alignment))
+  (txexpr 'div `((class ,class) (style ,style)) (map flex-item
+                                                     txexprs)))
 
 (define (empty-vspace #:size [size 4])
-    (define style (empty-space size 0 0 0))
-    (txexpr 'div style))
+  (define style (empty-space size 0 0 0))
+  (txexpr 'div style))
 
 (define (empty-space t b l r)
-    (define style (format "padding: ~aem ~aem ~aem ~aem;" t r b l))
-    `((style ,style)))
+  (define style (format "padding: ~aem ~aem ~aem ~aem;" t r b l))
+  `((style ,style)))
 
 (define (flex-item item)
-    item)
+  item)
