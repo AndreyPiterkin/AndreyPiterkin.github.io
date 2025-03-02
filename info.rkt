949af76aecbd63d971d6b79e4bff8bc927a06057
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-03-01 23:56:23 -0500

    another info.rkt file change

--- info.rkt
+++ info.rkt
@@ -1,7 +1,6 @@
 #lang info
 
-(define collection "personal-website")
+(define collection "andreypiterkin-personal-website")
 (define deps '("pollen" "colors"))
 (define pkg-desc "My implementation of my personal website with pollen")
 (define version "1.1")
-(define pkg-authors '(AndreyPiterkin))
