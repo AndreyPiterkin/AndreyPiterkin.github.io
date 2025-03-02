05384c9c95d08f47142bd627bf53bb9bdf09fa51
AndreyPiterkin <andrey.piterkin@gmail.com>
2025-03-02 04:58:28 +0000

    Deploying to gh-pages from @ AndreyPiterkin/AndreyPiterkin.github.io@32072f5fddd990ff2a2905539f3666edd3f9155a 🚀

--- styles.css
+++ styles.css
@@ -1,14 +1,93 @@
 #lang pollen
 
-:root {
+root {
 }
 
 /* Centering the page */
 body {
   margin: 3rem auto;
+  padding: 0;
   max-width: 750px; 
   font-size: 1.05em;
   font-family: 'Literata', serif;
+}
+
+.container {
+  min-height: 100%;
+}
+
+.template-content {
+  width: 100%;
+  /*
+  overflow-y: auto;
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  */
+}
+
+.footer {
+  text-align: center;
+  margin-top: auto;
+}
+
+
+/*
+.content::-webkit-scrollbar {
+  display: none; /* Chrome, Safari, Opera */
+}
+*/
+
+.statics {
+  margin-left: auto;
+  margin-right: 0;
+  margin-top: -1em;
+}
+
+.nav-item {
+  font-size: 1.3em;
+  font-weight: 590;
+  text-decoration: none;
+  transition: color 0.1s ease-in-out, transform 0.2s ease-in-out, text-decoration 0.1s ease-in-out;
+  display: inline-block; 
+}
+
+.nav-item:hover {
+  color: rgb(48,48,48);
+  transform: translateX(-2px);
+  text-decoration: underline;
+}
+
+.nav-item:hover:after {
+  content: " <";
+  text-decoration: none;
+  display: inline-block;
+  white-space: pre;
+}
+
+.nav-item:after {
+  content: " |";
+}
+
+.nav-item.active:after {
+  content: " <";
+  text-decoration: none;
+  display: inline-block;
+  white-space: pre;
+}
+
+.nav-item.active {
+  text-decoration: underline;
+}
+
+
+.nav-bar > .nav-item {
+  text-align: right;
+}
+
+.headline.active {}
+
+.main {
+  margin-top: 1em;
 }
 
 @media screen and (min-width: 1300px) {
@@ -18,18 +97,17 @@ body {
 
   .headline {
     white-space: nowrap;
+    float: right;
+    text-align: right;
   }
+  
 }
 
 @media screen and (max-width: 1299px) {
-  .headline {
+  .headline.* {
     white-space: normal;
+    text-align: right;
   }
-
-  .headline-box {
-    margin: 1em;
-  }
-
 }
 
 @media screen and (max-width: 900px) {
@@ -39,12 +117,68 @@ body {
 
   .headline-box {
     flex-direction: column !important;
-    margin: 2em;
   }
 
 
   .headline {
     white-space: nowrap;
+    text-align: center;
+  }
+
+  .statics {
+    margin: auto;
+  }
+
+  .main {
+    margin-top: 0em;
+  }
+
+  .nav-bar-flex {
+    flex-direction: row !important; 
+    column-gap: 1em !important;
+    margin: auto !important;
+    flex-wrap: wrap !important;
+    justify-content: center !important;
+  }
+
+  .nav-item {
+    font-size: 1.3em;
+    font-weight: 590;
+    text-decoration: none;
+    transition: text-decoration 0.1s ease-in-out !important;
+    display: block !important; 
+  }
+
+  .nav-item:hover {
+    color: rgb(48,48,48);
+    text-decoration: underline;
+    transform: translate(0px) !important;
+  }
+
+  .nav-item:hover:after {
+    content: "" !important; 
+    text-decoration: none !important;
+    display: block !important
+    white-space: pre !important;
+  }
+
+  .nav-item:after {
+    content: "" !important;
+  }
+
+  .nav-item.active:after {
+    content: "" !important;
+    text-decoration: none !important;
+    display: !important block;
+    white-space: !important pre;
+  }
+
+  .nav-item.active {
+    text-decoration: underline !important;
+  }
+
+  .nav-bar > .nav-item {
+    text-align: center !important;
   }
 
 }
@@ -116,3 +250,75 @@ ul.dash > li:before {
     display: table-cell;
 }
 
+.experience-block {
+}
+
+.experience-head {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.experience-title-name {
+  display: flex;
+  flex-direction: column;
+  row-gap: 0em;
+}
+
+.experience-name {
+  font: inherit;
+  font-size: 1.1em;
+  font-weight: 600;
+  font-style: normal;
+  margin-bottom: 0px;
+  margin-top: 0px;
+}
+
+
+.experience-title {
+  font: inherit;
+  font-size: 1.1em;
+  font-weight: 200;
+  font-style: italic;
+  margin-bottom: 0px;
+  margin-top: 0px;
+}
+
+
+.experience-date-range {
+  font: inherit;
+  font-size: 1em;
+  font-weight: 100;
+  font-style: italic;
+  margin-bottom: 0px;
+  margin-top: 0px;
+  text-align: right;
+  white-space: nowrap;
+}
+
+.experience-langs {
+  display: flex;
+  flex-direction: row;
+  column-gap: 1em;
+  align-items: center;
+}
+
+.lang {
+  display: flex;
+  flex-direction: row;
+  column-gap: 0.25em;
+  align-items: center;
+  margin-top: 0.4em;
+}
+
+.lang-name {
+  margin: 0;
+  display: inline;
+}
+
+.lang-dot {
+    display: inline-block;
+    width: 0.625rem;
+    height: 0.625rem;
+    border-radius: 50%;
+}
