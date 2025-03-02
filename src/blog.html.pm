038cc8383eaf51dddb743ccf10346bad9a627729
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-03-02 10:43:40 -0500

    wip: blog page

--- src/blog.html.pm
+++ src/blog.html.pm
@@ -1,3 +1,7 @@
 #lang pollen
 
-~p[#:style "text-align: center; margin-top: 10em;"]{Under construction...}
+~vempty[size 1.5]
+~flex[rows (gap 2) (class "main")]{
+    ~blog-desc["what's the fun in macros" "2025/03/02" (plangs "Racket" "HTML" "CSS")]
+    ~blog-desc["what's the fun in macros" "2025/03/02" (plangs "Racket")]
+}
