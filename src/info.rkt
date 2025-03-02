200064a6e099cc843fa75cdea86578806b9d180c
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-03-01 23:47:08 -0500

    chore: create info.rkt file

---
+++ src/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define collection "personal-website")
+(define deps '("pollen" "colors"))
+(define pkg-desc "My implementation of my personal website with pollen")
+(define version "1.1")
+(define pkg-authors '(AndreyPiterkin))
