3f058a34a852886ca56993c5e6616f82a2244732
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-03-14 16:30:44 -0400

    add dynamic blog.html.pm expansion

--- src/pollen.rkt
+++ src/pollen.rkt
@@ -14,7 +14,12 @@
                      racket/draw
                      racket/class
                      racket/path
-                     colors))
+                     colors
+                     "lang-coloring.rkt"))
+
+(module setup racket/base
+  (provide (all-defined-out))
+  (define command-char #\~))
 
 (provide (all-defined-out))
 
@@ -233,50 +238,3 @@
                   [(date ...) (extract-dates blog-posts)])
        #'(flex rows (gap 1) (rt:blog-desc 'title 'langs 'date) ...))]))
 
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; color utilities
-
-(begin-for-syntax
-
-  (define golden-ratio-conj 0.618033988749895)
-  (define sat 0.7)
-  (define val 0.8)
-  (define colors-file (build-path (current-directory) (string->path "colors.rktd")))
-  (define prev-hue 0.7)
-
-  (define (gen-color start-hue) 
-      (define hue start-hue)
-      (thunk
-        (set! hue (+ hue golden-ratio-conj))
-        (set! hue (- hue (floor hue)))
-        (set! prev-hue hue)
-        (define color (hsv->color (hsv hue sat val)))
-        (list (send color red) (send color green) (send color blue) (send color alpha))))
-  (define color-generator (gen-color 0.5))
-
-  (define lang-coloring-table 
-    (if (file-exists? colors-file)
-        (let* ([p (with-input-from-file colors-file (thunk (read)))]
-               [next-color (cdr p)]
-               [h (car p)])
-          (set! color-generator (gen-color next-color))
-          (make-hash (hash->list h)))
-        (make-hash)))
-
-  (define current-shared-colortable (make-parameter lang-coloring-table))
-
-  (define (add-coloring! lang)
-    (define lang^ (syntax->datum lang))
-    (if (hash-has-key? (current-shared-colortable) lang^)
-        (void)
-        (hash-set! (current-shared-colortable) lang^ (color-generator)))
-
-    (with-output-to-file colors-file
-      #:exists 'replace
-      (thunk (write (cons (current-shared-colortable) prev-hue))))))
-
-(module setup racket/base
-  (provide (all-defined-out))
-  (define command-char #\~))
