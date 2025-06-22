a0d7af1838f1dbdf68ce4ed2e69b26414b92f0bd
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-06-21 18:47:07 -0700

    reorder bullets

--- src/index.html.pm
+++ src/index.html.pm
@@ -6,10 +6,10 @@
 
     ~h3{Recently, I}
     ~items{
-        ~{Graduated from Northeastern with a degree in Computer Science!}
         ~{Worked at ~link[(url "https://www.mathworks.com")]{MathWorks} in ~em{Control Design Automation} for Spring 2024, improving Simulink C codegen by optimizing type selection}
         ~{Worked at ~link[(url "https://www.databricks.com")]{Databricks} on the ~em{Money} team for Summer 2024, improving load, integration, and chaos testing for the real-time billing pipeline}
         ~{Worked at ~link[(url "https://www.datadoghq.com")]{Datadog} on the ~em{Edge Ingress} team for Fall 2024, enabling search for automation-managed cloud resources}
+        ~{Graduated from Northeastern with a degree in Computer Science!}
     }
 
     ~vempty[size 2]
