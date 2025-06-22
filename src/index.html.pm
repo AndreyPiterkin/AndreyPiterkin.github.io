e1556c82f6375bc700fdc50c2d1330382d112fa9
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-06-21 18:42:36 -0700

    update home page

--- src/index.html.pm
+++ src/index.html.pm
@@ -1,21 +1,22 @@
 #lang pollen
 
 ~flex[rows (gap 0) (class "main")]{
-    ~p{I am Andrey, and I am a Senior studying Computer Science at Northeastern University. I am passionate about ~em{systems programming}, ~em{systematic design}, and ~em{simulation}.}
+    ~p{I am Andrey, and I am a Software Engineer passionate about ~em{systems programming}, ~em{systematic design}, and ~em{simulation}.}
     ~vempty[size 2]
 
-    ~h3{Recently, I was}
+    ~h3{Recently, I}
     ~items{
-        ~{Working at ~link[(url "https://www.mathworks.com")]{MathWorks} in ~em{Control Design Automation} for Spring 2024, improving Simulink C codegen by optimizing type selection}
-        ~{Working at ~link[(url "https://www.databricks.com")]{Databricks} on the ~em{Money} team for Summer 2024, improving load, integration, and chaos testing for the real-time billing pipeline}
-        ~{Working at ~link[(url "https://www.datadoghq.com")]{Datadog} on the ~em{Edge Ingress} team for Fall 2024, enabling search for automation-managed cloud resources}
+        ~{Graduated from Northeastern with a degree in Computer Science!}
+        ~{Worked at ~link[(url "https://www.mathworks.com")]{MathWorks} in ~em{Control Design Automation} for Spring 2024, improving Simulink C codegen by optimizing type selection}
+        ~{Worked at ~link[(url "https://www.databricks.com")]{Databricks} on the ~em{Money} team for Summer 2024, improving load, integration, and chaos testing for the real-time billing pipeline}
+        ~{Worked at ~link[(url "https://www.datadoghq.com")]{Datadog} on the ~em{Edge Ingress} team for Fall 2024, enabling search for automation-managed cloud resources}
     }
 
     ~vempty[size 2]
     ~h3{Right now, I am}
     ~items{
-        ~{Learning ~plang{Racket}'s macro system in Hack Your Own Language (CS 3620)}
-        ~{Taking Computer Architecture and Theory of Computation}
+        ~{Working on a procedural simulation of the world in ~plang{Rust}}
+        ~{Relaxing at home with my family and my dog, Ozzy}
     }
 
     ~vempty[size 2]
